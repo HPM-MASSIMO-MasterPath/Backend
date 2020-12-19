76540645d8ef446354162295010a131c6f300c96
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-12-19 02:28:03 -0600

    Fixing files corrupted

---
+++ database/models/categories.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const setupDatabase = require('../lib/db')
+
+module.exports = function setup (config) {
+  const sequelize = setupDatabase(config)
+
+  return sequelize.define('categories', {
+    category: {
+      type: Sequelize.STRING,
+      allowNull: true
+    }
+  })
+}
