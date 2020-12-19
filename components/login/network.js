eb14b57f8416154b6209452750a3637e24f12d14
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2020-12-19 03:03:29 -0600

    Adding login

--- components/login/network.js
+++ components/login/network.js
@@ -4,7 +4,7 @@ const controller = require('./index')
 
 const getOk = async (req, res) => {
   try {
-    const result = await controller.login(req.body) 
+    const result = await controller.login(req.body)
     res.status(200).json(result)
   } catch (err) {
     res.status(400).send(err)
